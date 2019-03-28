6809ebc6a7be1db202426c0a761a155343973988
Jhonatan Uribe <jhonatan.uribe@globant.com>
2019-03-28 09:54:37 -0500

    [WEBSITE] - Setting up Styles and Assets

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { OptionGroupComponent } from './components/option-group/option-group.component';
+import { RatingStarsComponent } from './components/rating-stars/rating-stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FiltersComponent,
+    OptionGroupComponent,
+    RatingStarsComponent
   ],
   imports: [
     BrowserModule,
